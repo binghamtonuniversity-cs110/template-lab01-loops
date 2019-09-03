992fae485fbe2c4120c5ad2b6392a467347c27ba
Steven Moore <stevenallenmoore@gmail.com>
2019-09-03 11:01:04 -0400

    Updated for Fall 19

--- lab2.py
+++ lab2.py
@@ -1,28 +1,28 @@
+import turtle               #1. import modules
+import random
 
-def main():
-    #Part A
-    window = turtle.Screen()       # 2.  Create a screen
-    window.bgcolor('lightblue')
+#Part A
+window = turtle.Screen()       # 2.  Create a screen
+window.bgcolor('lightblue')
 
-    michelangelo = turtle.Turtle()    # 3.  Create two turtles
-    leonardo = turtle.Turtle()
-    michelangelo.color('orange')
-    leonardo.color('blue')
-    michelangelo.shape('turtle')
-    leonardo.shape('turtle')
-
-
-    michelangelo.up()          # 4.  Pick up the pen so we don’t get lines
-    leonardo.up()
-    michelangelo.goto(-100,20)
-    leonardo.goto(-100,-20)
+michelangelo = turtle.Turtle()    # 3.  Create two turtles
+leonardo = turtle.Turtle()
+michelangelo.color('orange')
+leonardo.color('blue')
+michelangelo.shape('turtle')
+leonardo.shape('turtle')
 
 
-    # 5. your code goes here
+michelangelo.up()          # 4.  Pick up the pen so we don’t get lines
+leonardo.up()
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
 
 
-    # Part B - complete part B here
+# 5. your code goes here
 
 
-    window.exitonclick()
-main()
+# Part B - complete part B here
+
+
+window.exitonclick()
